9d7bb737bb304960055cc93458c3ece604f28f13
IcePickCybersolutions <hackhq81@gmail.com>
2021-04-05 12:03:59 -0600

    Update aharmonywindows.py

--- aharmonywindows.py
+++ aharmonywindows.py
@@ -11,7 +11,8 @@ print(r"""\
                                                |___/
 """)
 print('''
-Proudly a creation of IcePickCybersolutions
+Proudly a creation of IcePickCybersolutions.
+Visit my github repo for help, or to file a complaint.
 
 Run the script, answer a couple questions, and voila!
 You've automated a task!
@@ -19,8 +20,23 @@ You've automated a task!
 Upon the prompt simply tell me what you want to do.
 
 For example if you want to:
-open a file = openfile
-open a file with admin priveleges = adminopenfile
-use a hotkey = hotkey
-
+- open a file = openfile
+- open a file with admin priveleges = adminopenfile
+- use a hotkey = hotkey
+- type out something = type
+- search the web = search
+- create a file = createfile
+- run a python script = pyrun
+-
 ''')
+
+
+initiateprog = input("Do you want a task automated?: ")
+if initiateprog == yes or y:
+    filename = input("What should we name the file?: ")
+    # create a file with the name from filename
+
+    # continue to ask questions until task is automated
+    while continueorend == yes or y:
+        command = input("What must we do now?: ")
+        # save command to a file
