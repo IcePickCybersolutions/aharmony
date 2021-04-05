497c17b239da58b1e4633ce71acb89bcf3a439f2
IcePickCybersolutions <hackhq81@gmail.com>
2021-04-05 11:54:30 -0600

    Update aharmonywindows.py

--- aharmonywindows.py
+++ aharmonywindows.py
@@ -1,1 +1,26 @@
 import pyautogui
+import os
+
+print(r"""\
+       | |
+   __ _| |__   __ _ _ __ _ __ ___   ___  _ __  _   _
+  / _` | '_ \ / _` | '__| '_ ` _ \ / _ \| '_ \| | | |
+ | (_| | | | | (_| | |  | | | | | | (_) | | | | |_| |
+  \__,_|_| |_|\__,_|_|  |_| |_| |_|\___/|_| |_|\__, |
+                                                __/ |
+                                               |___/
+""")
+print('''
+Proudly a creation of IcePickCybersolutions
+
+Run the script, answer a couple questions, and voila!
+You've automated a task!
+
+Upon the prompt simply tell me what you want to do.
+
+For example if you want to:
+open a file = openfile
+open a file with admin priveleges = adminopenfile
+use a hotkey = hotkey
+
+''')
